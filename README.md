c329dfa7941aef484d25b2500e122832bb370ca1
MinGook <mgk8609@naver.com>
2021-09-21 23:03:33 +0900

    Router Setting

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # instaclone web
 
 ## Using pkages
+
 1. styled component : css를 하기 위해 사용
+
 2. React Hook Form : react 에서 form 작성을 쉽게 해줌
+
 3. React Router : SPA 구현을 위해
+   특정 url로 접근을 하면 어떤 component를 보여준다.
+   새로고침 되는게 아니라 컴포넌트만 업데이트 해주는거임
+
 4. Apoll Client v3 : backend 연동
 5. react helmet : title을 동적으로 변경이 가능하도록 만들어 줌
 6. react-fontawesome : 여러 아이콘 이모티콘을 사용하기 위해서
 
 ## Setup
 
-- [ ] Router
+- [x] Router
 - [ ] Authantication
 - [ ] Architechure
 - [ ] Styles
--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './screens/Home'
+import Login from './screens/Login'
+import NotFound from './screens/NotFound'
 
 function App() {
+  let isLogin = true
+
   return (
-    <div className="App"></div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {isLogin ? <Login /> : <Home />}
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
